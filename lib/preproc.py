c3c954945c71879f7a0da35ee501e4afa4e62c6c
James Santangelo <james.santangelo37@gmail.com>
2021-12-24 12:20:02 -0500

    Changing to Year instead of Date

--- lib/preproc.py
+++ lib/preproc.py
@@ -11,6 +11,7 @@ def match(string, candidates) -> str:
 def format_entry(entry: Dict[str, str], journals: List[Dict[str, str]], conferences: List[Dict[str, str]]) -> Dict[str, Dict[str, str]]:
     """ Produces a dictionary in format column_name: {type: x, value: y} for each value in the entry"""
     # Select the conference shortname based on proceedings
+    print(entry)
     if entry['Item type'] == 'Journal Article':
         if 'Full journal' in entry.keys() and entry['Full journal']:
             venue = [j['short'] for j in journals if j['name'] == entry['Full journal'].strip()]
@@ -44,11 +45,7 @@ def format_entry(entry: Dict[str, str], journals: List[Dict[str, str]], conferen
     for i, v in enumerate(venue):
         exclude = set([','])
         venue[i] = ''.join(ch for ch in v if ch not in exclude)
-    date = entry['Date published'].strip() if 'Date published' in entry.keys() else ''
-    if len(date) > 10:
-        date = date[:10]
-    elif len(date) == 4 or not date:
-        date = entry['Publication year'].strip() + '-01-01'
+    year = entry['Publication year'].strip()
     all_labels = [x.strip() for x in entry['Labels filed in'].strip().split(';')]
     fields = [x.split(' - ')[1] for x in all_labels if x.startswith('FLD')]
     methods = [x.split(' - ')[1] for x in all_labels if x.startswith('MET')]
@@ -59,7 +56,7 @@ def format_entry(entry: Dict[str, str], journals: List[Dict[str, str]], conferen
         'Authors':          {'type': 'multi_select', 'value': entry['Authors'].strip().split(',')},
         'Title':            {'type': 'title',        'value': entry['Title'].strip().replace('{','').replace('}','')},
         'Venues':           {'type': 'multi_select', 'value': venue},
-        'Date Published':   {'type': 'date',         'value': date},
+        'Year':             {'type': 'select',       'value': year},
         'Link':             {'type': 'url',          'value': selected_link},
         'Fields':           {'type': 'multi_select', 'value': fields},#, 'color': [COLOR_MAP[cat]['color'] for cat in categories]}
         'Methods':          {'type': 'multi_select', 'value': methods},
